9847902f0553387800a9afd7a8c2c5da38d486e3
Sebastien Dubois <seb@dsebastien.net>
2024-08-19 15:12:13 +0200

    Improved image generation modal. Closes #3

--- apps/plugin/src/app/modals/prompt-modal.ts
+++ apps/plugin/src/app/modals/prompt-modal.ts
@@ -6,34 +6,47 @@ import { App, Modal, Setting } from 'obsidian';
  */
 export class PromptModal extends Modal {
   result: string;
-  onSubmit: (result: string) => void;
 
-  constructor(app: App, onSubmit: (result: string) => void) {
+  constructor(app: App, private onSubmit: (result: string) => void) {
     super(app);
-    this.onSubmit = onSubmit;
     this.result = '';
+    this.modalEl.addClass('replicate-plugin-prompt-modal');
   }
 
   onOpen() {
     const { contentEl } = this;
 
-    contentEl.createEl('h1', { text: 'Enter the prompt' });
+    contentEl.createEl('h1', { text: 'Generate image(s) using Replicate.com' });
 
-    new Setting(contentEl).setName('Prompt').addText((text) =>
+    let submitButton: HTMLButtonElement;
+
+    new Setting(contentEl).setName('Prompt to use:').addTextArea((text) => {
       text.onChange((value) => {
         this.result = value;
-      })
-    );
+      });
+      text.setPlaceholder('Enter the prompt to use');
+      text.inputEl.addEventListener('keydown', (event) => {
+        if (event.ctrlKey && event.key === 'Enter') {
+          // Prevent default behavior if needed
+          event.preventDefault();
+          if (!submitButton) {
+            return;
+          }
+          submitButton.click();
+        }
+      });
+    });
 
-    new Setting(contentEl).addButton((btn) =>
+    new Setting(contentEl).addButton((btn) => {
+      submitButton = btn.buttonEl; // Store reference to the submit button
       btn
         .setButtonText('Submit')
         .setCta()
         .onClick(() => {
           this.close();
           this.onSubmit(this.result);
-        })
-    );
+        });
+    });
   }
 
   onClose() {
